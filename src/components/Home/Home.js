acccac77b2f6517259d279b803a58c773dad8b1e
thiwanka778 <thiwankaudumulla@gmail.com>
2023-06-24 18:26:57 +0530

    color effect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,28 +5,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Categories from "../../admin/Categories/Categories";
 import { getSlider, sliderReset } from "../../features/sliderSlice";
-import {getCategory} from "../../features/categorySlice";
+import { getCategory } from "../../features/categorySlice";
 import { whiteNotNeededPage } from "../../features/userSlice";
 
 const Home = () => {
   // console.log("catego", categorydata);
   const navigate = useNavigate();
-  const {categoryArray}=useSelector((state)=>state.category);
+  const { categoryArray } = useSelector((state) => state.category);
   const { slider, sliderLoading, sliderSuccess } = useSelector(
     (state) => state.slider
   );
   const dispatch = useDispatch();
   const { screen, user } = useSelector((state) => state.user);
- 
 
   React.useEffect(() => {
     dispatch(getSlider());
-   
+
     dispatch(getCategory());
-    
   }, []);
   React.useEffect(() => {
- 
     dispatch(whiteNotNeededPage());
   }, []);
 
@@ -40,7 +37,7 @@ const Home = () => {
     return <Categories key={item._id} {...item} usersHomePage={true} />;
   });
 
-  console.log(categoryDisplay)
+  console.log(categoryDisplay);
 
   return (
     <div className="home">
@@ -73,52 +70,18 @@ const Home = () => {
           marginBottom: "30px",
         }}
       >
-        
         <Sliderr />
       </div>
 
      
 
-      {/* <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "10px",
-          flexDirection: screen < 451 ? "column" : "row",
-        }}
-      >
-        <p
-          style={{
-            fontFamily: "'Poppins', sans-serif",
-            fontWeight: "bold",
-            fontSize: "20px",
-            letterSpacing: "3px",
-            marginBottom: screen < 451 ? "10px" : "0px",
-          }}
-        >
-          Choose What You Want
-        </p>
-
-        {user?.userType === "admin" && (
-          <button
-            style={{ marginLeft: screen < 451 ? "0px" : "20px" }}
-            className="type1"
-          >
-            <span onClick={() => navigate("/service")} className="btn-txt">
-              EDIT
-            </span>
-          </button>
-        )}
-      </div> */}
-
-
-
-      
+      <div className="typing-container">
+      <p className="typing-text">
+        Elevate Your Style with Our Premier Salon Services
+      </p>
+    </div>
 
       <div className="category-container">{categoryDisplay}</div>
-
-
     </div>
   );
 };
