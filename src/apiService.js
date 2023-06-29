97b914eca832dfd29838f0f75ac1b1ccb98a4c15
thiwanka778 <thiwankaudumulla@gmail.com>
2023-06-29 15:06:22 +0530

    firebase-URL

--- src/apiService.js
+++ src/apiService.js
@@ -1,3 +1,7 @@
-  export const BASE_URL="https://salonserver.onrender.com";
+ // export const BASE_URL="https://salonserver.onrender.com";
 
   // export const BASE_URL="http://localhost:8000";
+
+  //  export const BASE_URL="http://localhost:5001/thisara-salon/us-central1/app";
+
+   export const BASE_URL="https://us-central1-thisara-salon.cloudfunctions.net/app"
