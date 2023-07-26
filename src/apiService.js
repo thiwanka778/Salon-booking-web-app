85411b2fabe2f641f9aadf036eaf96af00adeb54
thiwanka778 <thiwankaudumulla@gmail.com>
2023-07-26 22:13:41 +0530

    api

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,7 @@
  // export const BASE_URL="https://salonserver.onrender.com";
 
-   export const BASE_URL="http://localhost:8000";
+  //  export const BASE_URL="http://localhost:8000";
 
   //  export const BASE_URL="http://localhost:5001/thisara-salon/us-central1/app";
 
-  //  export const BASE_URL="https://us-central1-thisara-salon.cloudfunctions.net/app"
+    export const BASE_URL="https://us-central1-thisara-salon.cloudfunctions.net/app"
